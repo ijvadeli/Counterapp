a2ddc77fe9bf6a213e7ab5c6a8a5383dedb4548e
Hitury <xxhituryxx@gmail.com>
2025-09-05 14:44:16 +0200

    Installed TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,16 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-
-  const handleDecrement = () => {
-    setCount(count - 1);
-  };
 
   return (
-    <div className="App">
+    <>
+    <div className="bg-gray-900">
       <h1>Counter App</h1>
       <p>Count: {count}</p>
       <button onClick={handleIncrement}>Add +</button>
       <button onClick={handleDecrement}>Subtract -</button>
     </div>
+    </>
   );
 }
 

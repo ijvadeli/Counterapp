f2d103448ee94bb6f6c72f34cf2847a852cc5e80
ijvadeli <idhvandelindt@icloud.com>
2025-09-05 16:31:49 +0200

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); {/*--- Create count and setCount variable ---*/}
   return (
     <>
       <div className="bg-[#081410] h-screen w-screen">
         <div className="flex justify-center items-center h-screen">
           <div className="card bg-[#193c2f] p-15 shadow-sm">
             <figure className="px-10 pt-10 flex justify-center mb-10">
+              {/*--- Count --*/}
               <h1>{count}</h1>
             </figure>
             <div className="card-body items-center text-center">
@@ -18,7 +19,9 @@ function App() {
                 Made by Dutch Restarts
               </p>
               <div className="card-actions flex justify-center gap-15">
+                {/*--- Add more to count ---*/}
                 <button className="btn btn-primary" onClick={() => setCount(count + 1)}>+</button>
+                {/*--- Subtract from count ---*/}
                 <button className="btn btn-primary" onClick={() => setCount(count - 1)}>-</button>
               </div>
             </div>

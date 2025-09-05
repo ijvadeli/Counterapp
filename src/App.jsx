cd544b4448fe2c4cfbefddd77a305968526c7bd0
Hitury <xxhituryxx@gmail.com>
2025-09-05 16:09:02 +0200

    UI Update

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,21 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bg-zinc-900 h-screen w-screen">
+      <div className="bg-[#081410] h-screen w-screen">
         <div className="flex justify-center items-center h-screen">
-          <div className="card bg-base-100 p-15 shadow-sm">
+          <div className="card bg-[#193c2f] p-15 shadow-sm">
             <figure className="px-10 pt-10 flex justify-center mb-10">
               <h1>0</h1>
             </figure>
             <div className="card-body items-center text-center">
-              <h2 className="card-title">React Counter App</h2>
-              <p>
+              <h2 className="card-title text-[32px]">React Counter App</h2>
+              <p className="mb-10">
                 Click the button below to add or decrease the counter!
                 Made by Dutch Restarts
               </p>
-              <div className="card-actions">
-                <button className="btn btn-primary">Buy Now</button>
+              <div className="card-actions flex justify-center gap-15">
+                <button className="btn btn-primary">+</button>
+                <button className="btn btn-primary hover:border-white">-</button>
               </div>
             </div>
           </div>

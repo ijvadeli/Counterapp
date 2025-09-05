d799999191d53c81835dd1907a331e5185e5a329
Hitury <xxhituryxx@gmail.com>
2025-09-05 15:49:06 +0200

    Added DaisyUI + Basic Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <div className="App">
-      <h1>Counter App</h1>
-    </div>
+      <div className="bg-zinc-900 h-screen w-screen">
+        <div className="flex justify-center items-center h-screen">
+          <div className="card bg-base-100 p-15 shadow-sm">
+            <figure className="px-10 pt-10 flex justify-center mb-10">
+              <h1>0</h1>
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">React Counter App</h2>
+              <p>
+                Click the button below to add or decrease the counter!
+                Made by Dutch Restarts
+              </p>
+              <div className="card-actions">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

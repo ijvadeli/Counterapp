91599aa8bffa90395ab2c605bf80660e2e86d3da
Hitury <xxhituryxx@gmail.com>
2025-09-05 14:55:29 +0200

    Fixed no display

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,8 @@ function App() {
 
   return (
     <>
-    <div className="bg-gray-900">
+    <div className="App">
       <h1>Counter App</h1>
-      <p>Count: {count}</p>
-      <button onClick={handleIncrement}>Add +</button>
-      <button onClick={handleDecrement}>Subtract -</button>
     </div>
     </>
   );
